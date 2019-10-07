a6a29106635844a60d3eb30ad57d38197236de4a
Jacky Feng <jackyfeng530@yahoo.com>
2019-10-07 19:29:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,25 @@
 class Application
-  
-  @@item
+
+  @@items = []
   
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
-      name = req.path.split("/items/").last
-      if item = @@items.find{|item| item.name == name}
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
         resp.write item.price
       else 
         resp.status = 400
         resp.write "Item not found"
       end
     else
-      resp.status = 404
+      resp.status=404
       resp.write "Route not found"
     end
-    
-  end 
-  
+    resp.finish
+  end
+
 end
+© 2019 GitHub, Inc.
