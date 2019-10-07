c063be0a6caeae2de7de8fbedb3868103b00fc87
Jacky Feng <jackyfeng530@yahoo.com>
2019-10-07 19:24:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,12 @@
 class Application
   
+  @@item
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path == "/"
+    if req.path == "/items/items.name"
     
   end 
   
